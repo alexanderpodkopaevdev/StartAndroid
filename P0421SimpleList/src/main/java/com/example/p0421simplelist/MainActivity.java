19c8ae65239fbbac5936a7b3514a986bdb9cac90
Alexander Podkopaev <p-alex1990@mail.ru>
2019-11-20 14:13:32 +0700

    Startandroid 42 lesson

---
+++ P0421SimpleList/src/main/java/com/example/p0421simplelist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.p0421simplelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lvMain);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.my_list_item,names);
+        listView.setAdapter(adapter);
+    }
+}
