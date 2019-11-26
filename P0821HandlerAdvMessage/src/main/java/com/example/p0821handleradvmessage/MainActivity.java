77054d2a713aeaadad0e8b74f56e38ecd2b5d7e9
Alexander Podkopaev <p-alex1990@mail.ru>
2019-11-26 14:52:18 +0700

    Startandroid 82 lesson

---
+++ P0821HandlerAdvMessage/src/main/java/com/example/p0821handleradvmessage/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.p0821handleradvmessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    final int STATUS_NONE = 0; // нет подключения
+    final int STATUS_CONNECTING = 1; // подключаемся
+    final int STATUS_CONNECTED = 2; // подключено
+    final int STATUS_DOWNLOAD_START = 3; // загрузка началась
+    final int STATUS_DOWNLOAD_FILE = 4; // файл загружен
+    final int STATUS_DOWNLOAD_END = 5; // загрузка закончена
+    final int STATUS_DOWNLOAD_NONE = 6; // нет файлов для загрузки
+
+    Handler h;
+
+    TextView tvStatus;
+    ProgressBar pbDownload;
+    Button btnConnect;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvStatus = findViewById(R.id.tvStatus);
+        pbDownload = findViewById(R.id.pbDownload);
+        pbDownload.
+        btnConnect = findViewById(R.id.btnConnect);
+
+        h = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case STATUS_NONE:
+                        btnConnect.setEnabled(true);
+                        tvStatus.setText("Not connected");
+                        pbDownload.setVisibility(View.GONE);
+                        break;
+                    case STATUS_CONNECTING:
+                        btnConnect.setEnabled(false);
+                        tvStatus.setText("Connecting");
+                        break;
+                    case STATUS_CONNECTED:
+                        tvStatus.setText("Connected");
+                        break;
+                    case STATUS_DOWNLOAD_START:
+                        tvStatus.setText("Start download " + msg.arg1 + " files");
+                        pbDownload.setMax(msg.arg1);
+                        pbDownload.setProgress(0);
+                        pbDownload.setVisibility(View.VISIBLE);
+                        break;
+                    case STATUS_DOWNLOAD_FILE:
+                        tvStatus.setText("Downloading. Left " + msg.arg2 + " files");
+                        pbDownload.setProgress(msg.arg1);
+                        saveFile((byte[]) msg.obj);
+                        break;
+                    case STATUS_DOWNLOAD_END:
+                        tvStatus.setText("Download complete!");
+                        break;
+                    case STATUS_DOWNLOAD_NONE:
+                        tvStatus.setText("No files for download");
+                        break;
+                }
+            }
+        };
+        h.sendEmptyMessage(STATUS_NONE);
+    }
+
+
+
+    public void onclick(View view) {
+        Thread t = new Thread(new Runnable() {
+            Message msg;
+            byte[]  file;
+            Random rand = new Random();
+            @Override
+            public void run() {
+                try {
+                    h.sendEmptyMessage(STATUS_CONNECTING);
+                    TimeUnit.SECONDS.sleep(1);
+
+                    h.sendEmptyMessage(STATUS_CONNECTED);
+                    TimeUnit.SECONDS.sleep(1);
+
+                    int filesCount = rand.nextInt(5);
+
+                    if (filesCount == 0) {
+                        h.sendEmptyMessage(STATUS_DOWNLOAD_NONE);
+                        TimeUnit.MILLISECONDS.sleep(1500);
+                        h.sendEmptyMessage(STATUS_NONE);
+                        return;
+                    }
+
+                    msg = h.obtainMessage(STATUS_DOWNLOAD_START,filesCount,0);
+                    h.sendMessage(msg);
+
+                    for (int i = 1; i <= filesCount; i++) {
+                        file = downloadFile();
+                        msg = h.obtainMessage(STATUS_DOWNLOAD_FILE,i,filesCount-i,file);
+                        h.sendMessage(msg);
+                    }
+                    h.sendEmptyMessage(STATUS_DOWNLOAD_END);
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                    h.sendEmptyMessage(STATUS_NONE);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+    }
+
+    private byte[] downloadFile() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+        return new byte[1024];
+    }
+    private void saveFile(byte[] obj) {
+    }
+}
