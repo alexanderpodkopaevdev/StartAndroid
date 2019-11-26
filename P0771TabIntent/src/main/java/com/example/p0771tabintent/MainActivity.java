84232c613eb19b4ecd389a7688cc3144e54972b8
Alexander Podkopaev <p-alex1990@mail.ru>
2019-11-26 10:39:42 +0700

    Startandroid 77 lesson

---
+++ P0771TabIntent/src/main/java/com/example/p0771tabintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.p0771tabintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpec;
+
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(new Intent(this, OneActivity.class));
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setContent(new Intent(this, TwoActivity.class));
+        tabHost.addTab(tabSpec);
+    }
+}
